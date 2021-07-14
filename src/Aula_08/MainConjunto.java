03819f747a37607b192a220d0c1431abf0be7ed6
unknown <deividalmeida365@gmail.com>
2021-07-13 23:14:55 -0300

    new features

--- src/Aula_08/MainConjunto.java
+++ src/Aula_08/MainConjunto.java
@@ -5,22 +5,48 @@ import java.util.List;
 import java.util.Scanner;
 import java.io.*;
 import java.nio.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 
 import Aula_07.Lista;
 
 
 public class MainConjunto {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		
 		 Scanner dadosUsuario = new Scanner(System.in);
 		 
 		 int posicao = 0;
 		 
+		 Lista lista = new Lista();
+		 String caminho = "registro-log.txt";
+		 
+		 File arq = new File(caminho);
+		 
+	     LocalDateTime ld = LocalDateTime.now();
+	     
+	     ld.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+	     
+	     try {
+	            if (arq.createNewFile()) {
+	                System.out.println("arquivo criado com sucesso");
+	            }
+	            
+	        } catch (IOException e){
+	        	
+	            System.out.println(e.getMessage());
+	            
+	        }
+	     
+	     
+	     RegistroLog registroLog = new RegistroLog();
+	     
+	  
+		 
 		 boolean setMenu = true;
-
-	     Lista lista = new Lista();
 	     
 	     
 
@@ -36,68 +62,86 @@ public class MainConjunto {
 	            
 	            case 1:
 	            	
-	            	try {
-	            		
-	            	File arq = new File("registro-log.txt");
+	            	System.out.println("Inserir Produto: ");
+	            	String produto = dadosUsuario.next();
+	            	lista.addProduto(produto);
+	            	
+	            	registroLog.escreveArq(caminho, ld, posicao, produto);
 	            	
 	            	
-	            	if(arq.exists() == false) {
-	            		
-	            		arq.createNewFile();
-	            		System.out.println("Arquivo criado!");
-	            	}
-	            	
-	            		
-	           		 System.out.println("Inserir Produto: ");
-	        		 String produto = dadosUsuario.next();
-
-	        			        				
-	        		 FileWriter escrever = new FileWriter(arq);
-	        		 BufferedWriter escreverBuffer = new BufferedWriter(escrever);
-	        		
-	        		 
-	        		 
-	        			
-	        			 escreverBuffer.write(produto);
-	        		 
-
-	        			
-	        			escreverBuffer.close();
-	        			escrever.close();
-	            	
-	        			
-	        			
-	            	} catch(IOException e) {
-	            		
-	            		System.out.println(e.getMessage());
-	            	}
-	        			
-
+	            
 	            	break;
 	            	
+	              	
 	            case 2:
+	            	
+	            	System.out.println("Inserir produto em posição específica: ");
+	            	produto = dadosUsuario.next();
+	            	lista.addProduto(produto);
+	            	
+	            	registroLog.escreveArq(caminho, ld, posicao, produto);
+	            	
 	            	break;
 	            	
 	            case 3:
-	            	break;
+	            	
+	            	System.out.println("Consultar produto (pelo nome)");
+	            	produto = dadosUsuario.next();
+	            	if(lista.consultaNome(produto)) {
+	                    System.out.println("item disponível" + produto);
+
+	            	} else {
+	                    System.out.println("item não disponíel");
+
+	            	}
 	            	
 	            case 4:
-	            	break;
 	            	
+	                posicao = dadosUsuario.nextInt();
+	                System.out.println(lista.consultaPos(posicao));
+	                
+	                
 	            case 5:
+	            	
+	            	System.out.println("Substituir produto: ");
+	            	produto = dadosUsuario.next();
+	            	lista.addProduto(produto);
+	            	
+	            	lista.subProduto(posicao, produto);
+	            	
 	            	break;
 	            	
 	            case 6:
+	            	
+	            	System.out.println("Remover Produto: ");
+	            	produto = dadosUsuario.next();
+	            	lista.rmvProduto(produto);
+	            	
 	            	break;
 	            	
 	            case 7:
+	            	
+	            	System.out.println("Remover produto (pela posição): ");
+	            	produto = dadosUsuario.next();
+	            	lista.rmvPosicao(posicao);
 	            	break;
 	            	
 	            case 8:
 	            	
+	            	System.out.println("Mostrar tudo!");
+	            	produto = dadosUsuario.next();
+	            	System.out.println(lista.getProd());
+	            	
+	            	break;
+	            	
+	            	
+	          
+	            case 0: 
+	            	
 	            	setMenu = false;
 	            	
 	           }
+	            
 
 	            
 	     	}
--- src/Aula_08/RegistroLog.java
+++ src/Aula_08/RegistroLog.java
@@ -1,13 +1,21 @@
 package Aula_08;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class RegistroLog {
+	
+	private File Log;
+	
+	  public void escreveArq(String b, LocalDateTime ld, int n, String produto) throws IOException {
+	        FileWriter fw = new FileWriter(b,true);
+	        BufferedWriter bw = new BufferedWriter(fw);
+	        bw.write(" -> Seu ID é: " + ld + " Você solicitou a inserção de produto em uma posição específica\n" + n + " Nome do Produto:\n " + produto);
+	        bw.close();
+	    }
 	
 	
 }
